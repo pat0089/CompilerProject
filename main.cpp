eb1c2b16114ebeef0330641287bded3df4c80999
pat0089 <21283826+pat0089@users.noreply.github.com>
2022-02-07 16:16:16 -0800

    File input:
    Intermediary commit on the way to making the code generator, can now use command line args for file input

--- main.cpp
+++ main.cpp
@@ -12,29 +12,15 @@ using std::string;
 
 int main(int argc, char * argv[]) {
 
-    std::ifstream in;
+    std::ifstream fin;
 
     if (argc > 1) {
-
-        in.open(argv[1]);
-        if (in.fail()) {
-            return -1;
-        }
-        Compiler.Lex(in);
-        in.close();
-    } else {
-        //most basic program, return a value:
-        string return_2 = "int main() {    \n"
-                          "    return 2;   \n"
-                          "}               ";
-
-        Compiler.Lex(return_2);
-        cout << "********************" << endl;
-        cout << "Original:" << endl;
-        cout << return_2 << endl;
-        cout << "********************" << endl;
+        fin.open(argv[1]);
     }
 
+    std::istream &in = fin.is_open() ? fin : std::cin;
+
+    Compiler.Lex(in);
     cout << "Split by tokens:" << endl;
     cout << Compiler.GetLexer();
     cout << endl << "********************" << endl;
@@ -42,22 +28,7 @@ int main(int argc, char * argv[]) {
     Compiler.Parse();
     cout << Compiler.GetAST();
 
-    string outString;
 
-    std::ofstream out;
-
-    if (argc > 1) {
-
-        //get path from argv[1]
-        string folder = string(argv[1]);
-        auto lastFolderOffset = folder.find_last_of('/');
-        folder = folder.substr(0, lastFolderOffset);
-        string name = string(argv[1]);
-        name.substr(lastFolderOffset+1);
-
-    } else {
-
-    }
 
     return 0;
 }
